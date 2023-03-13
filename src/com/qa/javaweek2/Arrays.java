5e2fb81efc2306252a4c5394b853cb904899fb9d
jgdavid <Jdavid@qa.com>
2023-03-13 15:18:49 +0000

    adding arrays and loops files

---
+++ src/com/qa/javaweek2/Arrays.java
@@ -0,0 +1,11 @@
+package com.qa.javaweek2;
+
+public class Arrays {
+	public static void main(String[] args) {
+		int[] array = new int[10];
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/com/qa/javaweek2/Loops.java
@@ -0,0 +1,46 @@
+package com.qa.javaweek2;
+
+public class Loops {
+	public static void main(String[] args) {
+
+//		Flow chart exercise 1
+//		for (int a = 100; a < 200; a++)
+//			System.out.println(a);
+//		Flow chart exercise 2
+
+//		for (int a = 100; a <= 200; a++)
+//			if (a % 2 == 0)
+//				System.out.println("-");
+//			else
+//				System.out.println("*");
+
+//		Flow chart exercise 3
+//		for (int a = 0; a < 11; a++) {
+//			for (int j = 0; j < 11; j++)
+//				System.out.println(a);
+//		}
+
+//		Flow chart exercise 5
+
+		for (int a = 1; a < 11; a++) {
+
+			if (a == 2)
+				System.out.println(a);
+			System.out.println(a);
+			if (a == 3)
+				System.out.println(a);
+			System.out.println(a);
+			System.out.println(a);
+
+		}
+
+//		Coins
+	}
+
+//	public static int Count() {
+//		for (int i = 0; i < 11; i++)
+//			System.out.println(i);
+//
+//	}
+
+}
