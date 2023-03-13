724c4be10a601a77d07d34b08f4f03ace65f5575
jgdavid <Jdavid@qa.com>
2023-03-13 16:29:05 +0000

    numbers 1

--- src/com/qa/javaweek2/Arrays.java
+++ src/com/qa/javaweek2/Arrays.java
@@ -1,11 +1,45 @@
 package com.qa.javaweek2;
 
 public class Arrays {
+
 	public static void main(String[] args) {
+
+
+
+//		Excercise 1
+
 		int[] array = new int[10];
 
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(i);
 		}
+
+//		Excercise 2
+
+		int[] nums = new int[10];
+
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(i);
+			for (int j = 0; j < nums.length; j++) {
+				System.out.println(j);
+			}
+		}
+		
+//		Numbers Excercise 1
+
+		int sum = 74;
+		int num = sum % 10;
+		int num1 = sum / 10;
+
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num + num1);
+		
+//		Numbers Excercise 2
+		
+		int[]
+		
+
 	}
+
 }
